f741aba5ef18000953636a8e6dc5532b109fb908
Udara Chandimal Sathmina <87545327+Chandi-99@users.noreply.github.com>
2023-09-14 11:55:48 +0530

    Add files via upload

---
+++ Average Form/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Average_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtmarksubject1 = new System.Windows.Forms.TextBox();
+            this.txtmarksubject2 = new System.Windows.Forms.TextBox();
+            this.txtAverage = new System.Windows.Forms.TextBox();
+            this.btnsubmit = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider2 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider3 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.labelMark = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(49, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Subject 1*";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.label2.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(49, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Subject 2*";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label3.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(49, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Average";
+            // 
+            // txtmarksubject1
+            // 
+            this.txtmarksubject1.Location = new System.Drawing.Point(152, 44);
+            this.txtmarksubject1.Name = "txtmarksubject1";
+            this.txtmarksubject1.Size = new System.Drawing.Size(177, 20);
+            this.txtmarksubject1.TabIndex = 3;
+            this.txtmarksubject1.Validating += new System.ComponentModel.CancelEventHandler(this.txtmarksubject1_validating);
+            // 
+            // txtmarksubject2
+            // 
+            this.txtmarksubject2.Location = new System.Drawing.Point(152, 77);
+            this.txtmarksubject2.Name = "txtmarksubject2";
+            this.txtmarksubject2.Size = new System.Drawing.Size(177, 20);
+            this.txtmarksubject2.TabIndex = 4;
+            this.txtmarksubject2.Validating += new System.ComponentModel.CancelEventHandler(this.txtmarksubject2_validating);
+            // 
+            // txtAverage
+            // 
+            this.txtAverage.Location = new System.Drawing.Point(152, 114);
+            this.txtAverage.Name = "txtAverage";
+            this.txtAverage.Size = new System.Drawing.Size(177, 20);
+            this.txtAverage.TabIndex = 5;
+            this.txtAverage.Validating += new System.ComponentModel.CancelEventHandler(this.txtResult_validating);
+            // 
+            // btnsubmit
+            // 
+            this.btnsubmit.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnsubmit.Font = new System.Drawing.Font("Cambria", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsubmit.Location = new System.Drawing.Point(197, 178);
+            this.btnsubmit.Name = "btnsubmit";
+            this.btnsubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnsubmit.TabIndex = 6;
+            this.btnsubmit.Text = "Submit";
+            this.btnsubmit.UseVisualStyleBackColor = false;
+            this.btnsubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // errorProvider2
+            // 
+            this.errorProvider2.ContainerControl = this;
+            // 
+            // errorProvider3
+            // 
+            this.errorProvider3.ContainerControl = this;
+            // 
+            // labelMark
+            // 
+            this.labelMark.AutoSize = true;
+            this.labelMark.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.labelMark.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMark.Location = new System.Drawing.Point(149, 148);
+            this.labelMark.Name = "labelMark";
+            this.labelMark.Size = new System.Drawing.Size(41, 13);
+            this.labelMark.TabIndex = 7;
+            this.labelMark.Text = "Grade";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(393, 213);
+            this.Controls.Add(this.labelMark);
+            this.Controls.Add(this.btnsubmit);
+            this.Controls.Add(this.txtAverage);
+            this.Controls.Add(this.txtmarksubject2);
+            this.Controls.Add(this.txtmarksubject1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Aggregate Function-Average";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtmarksubject1;
+        private System.Windows.Forms.TextBox txtmarksubject2;
+        private System.Windows.Forms.TextBox txtAverage;
+        private System.Windows.Forms.Button btnsubmit;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.ErrorProvider errorProvider2;
+        private System.Windows.Forms.ErrorProvider errorProvider3;
+        private System.Windows.Forms.Label labelMark;
+    }
+}
+
---
+++ Average Form/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Average_Form
+{
+    public partial class Form1 : Form
+    {//declare variables
+        private int MarkSubject1;
+        private int MarkSubject2;
+        private int result;
+
+        private int intvalidation;
+
+        //declare and initialize variables
+        private bool bsubject1 = false;
+        private bool bsubject2 = false;
+        private bool bresult = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //initialise variables  on form constructors
+            MarkSubject1 = 0;
+            MarkSubject2 = 0;
+
+            //change attribute
+            txtAverage.ReadOnly = true;
+
+        }
+
+        private void txtmarksubject1_validating(object sender, CancelEventArgs e)
+        {//clear error provider
+            errorProvider1.SetError(txtmarksubject1, "");
+            bsubject1 = false;
+            if (!int.TryParse(txtmarksubject1.Text, out intvalidation))
+            {
+                bsubject1 = true;
+                errorProvider1.SetError(txtmarksubject1, "Please fill the required area");
+            }
+
+        }
+
+        private void txtmarksubject2_validating(object sender, CancelEventArgs e)
+        {//clear error provider
+            errorProvider2.SetError(txtmarksubject2, "");
+            bsubject2 = false;
+            if (!int.TryParse(txtmarksubject2.Text, out intvalidation))
+            {
+                bsubject2 = true;
+                errorProvider2.SetError(txtmarksubject2, "Please fill the required area");
+            }
+
+        }
+
+        private void txtResult_validating(object sender, CancelEventArgs e)
+        {//clear error provider
+            errorProvider3.SetError(txtAverage, "");
+            bresult = false;
+            if (!int.TryParse(txtAverage.Text, out intvalidation))
+            {
+                bresult = true;
+                errorProvider3.SetError(txtAverage, "Please fill the required area");
+            }
+
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (bsubject1 == false && bsubject2 == false)
+                {
+                    MarkSubject1 = int.Parse(txtmarksubject1.Text);
+                    MarkSubject2 = int.Parse(txtmarksubject2.Text);
+
+                    result = ((MarkSubject1 + MarkSubject2) / 2);
+                    txtAverage.Text = result.ToString();
+                    if (result >= 0 && result < 40)
+                    {
+                        labelMark.Text = "Grade F";
+                    }
+                    else if (result >= 40 && result < 65)
+                    {
+                        labelMark.Text = "Grade C";
+
+                    }
+                    else if (result >= 65 && result < 75)
+                    {
+                        labelMark.Text = "Grade B";
+                    }
+                    else if (result >= 75 && result <= 100)
+                    {
+                        labelMark.Text = "Grade A";
+                    }
+                    else if (result > 100)
+                    {
+                        labelMark.Text = "Invalid Mark Submited";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
+    
+
